5dec444fcf60f69ccb2fb745b9656c45c07548c2
HoangSon <son.lyhoang2014@gmail.com>
2023-07-24 13:35:23 +0700

    FE-Son: Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import { publicRoutes } from '~/routes';
+import DefaultLayout from './layouts/DefaultLayout';
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                path={route.path}
+                                key={index}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
